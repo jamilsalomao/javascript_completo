dbc5a1d49b4db3cfe5fa7ec08000868f215b041a
Jamil Salomão <132591829+jamilsalomao@users.noreply.github.com>
2024-10-03 12:47:31 -0300

    Mais uma seção
    Mais uma etapa do curso de JavaScript

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -127,5 +127,92 @@ if(userName === "Ronald") {
 
 // 10 - while
 
- 
+let p = 0
 
+while(p < 5) {
+    console.log(`Repetindo ${p}`)
+    p = p +1;
+}
+
+// 11 - do while
+let o = 10
+
+do {
+    console.log(`Valor de o: ${o}`);
+    o--;
+} while (o > 1);
+
+// 12 - for
+for(let t = 0; t < 10; t++) {
+    console.log("Repetindo algo...");
+}
+
+let r = 10
+
+for (r; r > 0; r = r - 1) {
+    console.log(`O r está diminuindo: ${r}`);
+}
+
+// 13 - identação
+for(let u = 0; u < 10; u++) {
+    if(u * 2 >10) {
+        console.log(`Maior que 10! ${u}`);    
+    } else {
+        if (u / 2 === 0) {
+            console.log("deu 0");
+        }
+    }
+}
+
+// 14 - break
+for (let g =20; g >10; g--) {
+    console.log(`O valor de g é: ${g}`);
+
+    if(g === 12) {
+        console.log("O g é 12!");
+        break;
+    } 
+}
+
+// 15 - continue
+for(let s = 0; s < 10; s = s + 1) {
+    // operador resto = %
+    if(s  % 2 === 0) {
+        console.log("Número par!");
+        continue;
+    }
+
+    console.log(s);
+}
+
+// 16 - switch
+const job = "Analista"
+
+switch(job) {
+    case "Programador":
+        console.log("Você é um programador!");
+        break;
+    case "Advogado":
+        console.log("Você é um advogado!");
+        break;
+    case "Engenheiro":
+        console.log("Você é um engenheiro!");
+        break;
+    default:
+        console.log("Profissão não encontrada");
+}
+
+// Switch "errado"
+const l = 100
+
+switch(l) {
+    case 200:
+        console.log("L é 200!");
+    case 100:
+        console.log("L é 100!");
+    case 10:
+        console.log("L é 10!");
+    default:
+        console.log("L não foi encontrado!");
+        
+}
